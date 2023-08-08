02ccf154862eec31fc3185b0ad7dfc7c33ed24da
Lili Wilson <lmwilson@mit.edu>
2023-08-08 11:26:54 -0400

    added links to experience

--- src/components/Position.js
+++ src/components/Position.js
@@ -5,29 +5,46 @@ import {
   CardOverflow,
   AspectRatio,
   Typography,
+  Box,
 } from "@mui/joy";
+import LinkIcon from "@mui/icons-material/Link";
 
 const Position = (props) => {
   return (
-    <Card orientation="horizontal" sx={{ width: "90%"}}>
+    <Card orientation="horizontal" sx={{ width: "90%" }}>
       <CardOverflow sx={{ margin: "0" }}>
         <AspectRatio ratio="1" sx={{ width: 80 }}>
-          <img src={process.env.PUBLIC_URL + `/positions/${props.logo}`} loading="lazy" alt="" />
+          <img
+            src={process.env.PUBLIC_URL + `/positions/${props.logo}`}
+            loading="lazy"
+            alt=""
+          />
         </AspectRatio>
       </CardOverflow>
       <CardContent align="left">
-        <Typography level="h2" fontSize="lg" textColor="#000" mb={1}>
-          {props.position} •{" "}
-          <Typography
-            level="h3"
-            fontStyle={"italic"}
-            fontSize="lg"
-            textColor="#000"
-            mb={1}
-          >
-            {props.place}
+        <Box display="flex">
+          <Typography level="h2" fontSize="lg" textColor="#000" mb={1}>
+            {props.position} •{" "}
+            <Typography
+              level="h3"
+              fontStyle={"italic"}
+              fontSize="lg"
+              textColor="#000"
+              mb={1}
+            >
+              {props.place}
+            </Typography>
           </Typography>
-        </Typography>
+          <a
+            href={props.link}
+            rel="noreferrer"
+            target="_blank"
+            style={{ color: "black", paddingLeft: "0.5em" }}
+          >
+            <LinkIcon />
+          </a>
+        </Box>
+
         <Typography>{props.description}</Typography>
       </CardContent>
     </Card>
